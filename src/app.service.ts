0054e1b24af6e9313001bb04899553e7b1b0a946
Volodymyr Savko <v.savko@dinamicka.com>
2022-09-01 23:45:34 +0300

    trying to connect to Api quality data platform

--- src/app.service.ts
+++ src/app.service.ts
@@ -18,8 +18,10 @@ export class AppService {
   async getHello(): Promise<any> {
     // const url = 'http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json';
     // const { data } = await this.httpService.axiosRef.get("http://localhost:5001/users");
+    const url = "https://api.waqi.info/feed/here/?token=815b67490e5fe3c87b35b40610aa7b37abb4908a"
     const response =
-        await this.httpService.axiosRef.get("http://localhost:5001/users")
+        // await this.httpService.axiosRef.get("http://localhost:5001/users")
+        await this.httpService.axiosRef.get(url)
     console.log({response})
     return response?.data;
   }
